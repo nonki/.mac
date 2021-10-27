5a95383d517956b4f7cf6ab1657947262177ce30
nonki <thomas@flack.it>
2021-10-27 13:54:23 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,19 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install vim go thefuck rbenv pass keychain node@14 rg tmux java jenv python assh zsh gnupg openjdk pygments kind kubectl kustomize kubebuilder
+brew install node@14
 
 brew link node@14 --force
 
-ssh-keygen -t ed25519 -C "thomas@flack.it"
+brew install vim go thefuck rbenv pass keychain rg tmux java jenv python assh zsh gnupg openjdk pygments kind kubectl kustomize kubebuilder
+
+read -p 'Generate new key? [y/N]: ' shouldGenerateKey
+
+shouldGenerateKey=${shouldGenerateKey:-"N"}
+
+if [ shouldGenerateKey == 'y' ]; then
+  ssh-keygen -t ed25519 -C "thomas.flack@skybettingandgaming.com"
+fi
 
 read -p 'Path to key [~/.ssh/id_ed25519]: ' keypathvar
 keypathvar=${keypathvar:-"~/.ssh/id_ed25519"}
@@ -19,7 +27,7 @@ ssh-add -K $keypathvar
 
 /bin/bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-chsh -s $(which zsh)
+pbcopy < $keypathvar
 
 read -p 'Continue once uploaded key to Github [y]: ' contvar
 
@@ -44,3 +52,5 @@ git clone git@github.com:nonki/.zsh.git
 # pass init "GPG KEY ID"
 # cd /tmp && go install golang.org/x/tools/gopls@latest
 # ssh-keyscan -t rsa stash.skybet.net >> ~/.ssh/known_hosts
+
+chsh -s $(which zsh)
