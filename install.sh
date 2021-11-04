c37fb65f10c8dafbf88314ca55b3271a3b3fce7f
nonki <thomas@flack.it>
2021-11-04 14:37:16 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ brew install node@14
 
 brew link node@14 --force
 
+brew tap kudobuilder/tap
 brew install vim go thefuck rbenv pass keychain rg tmux java jenv python assh zsh gnupg openjdk pygments kind kubectl kustomize kubebuilder kuttl-cli
 
 read -p 'Generate new key? [y/N]: ' shouldGenerateKey
