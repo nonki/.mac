ba0365a26067c465cd2501d9280ecfa73769bbcf
nonki <thomas@flack.it>
2021-11-25 16:31:47 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -66,4 +66,8 @@ ln -s ~/code/github.com/nonki/.ssh/assh.yml ~/.ssh/assh.yml
 
 ~/.vim/install.sh
 
+git config --global user.name "Thomas Flack"
+git config --global user.email "thomas.flack@skybettingandgaming.com"
+git config --global core.hooksPath ~/code/github.com/nonki/.gitconfig/hooks
+
 /bin/bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" # took me into seperate shell
